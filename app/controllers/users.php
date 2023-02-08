e37a83dab111b7274593fb79717ecd24ac02c9e0
olga <goncherenkoola@gmail.com>
2023-02-08 19:01:18 +0200

    users edit update

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -3,8 +3,11 @@
  include(ROOT_PATH.'/app/database/db.php');
  include(ROOT_PATH. '/app/helpers/validateUser.php');
  
-
+ $table='users';
+ 
+ $admin_users=selectAll($table,['admin'=>1]);
  $errors=array();
+ $id='';
  $username='';
  $email='';
  $password='';
@@ -17,7 +20,7 @@ if( isset($_POST['register-btn']) ){
    
   
     
-    if(count($errors)===0) {
+    if(count($errors)==0) {
         unset($_POST['register-btn'],$_POST['passwordConf']);
         $_POST['admin']=1;
     
@@ -51,10 +54,96 @@ if( isset($_POST['register-btn']) ){
 
  }
 
+
+ if( isset($_POST['register-btn'])|| isset($_POST['create-admin'] )){
+   $errors=validateUser($_POST);
+   
+    if(count($errors)==0) {
+        unset($_POST['register-btn'],$_POST['passwordConf'],$_POST['create-admin']);
+        $_POST['password']=password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+        if(isset($_POST['admin'])){
+         $_POST['admin']=1;
+         $user_id=create($table,$_POST);
+         $_SESSION['message']='Admin user created';
+         $_SESSION['type']="Success";
+         header("location: ".BASE_URL.'/admin/users/index.php');
+         exit();
+
+        }else{
+         $_POST['admin']=0;
+         $user_id=create($table,$_POST);
+         $user=selectOne($table,['id'=>$user_id]);
+        loginUser($user);
+        }
+        }
+        else{
+         $username=$_POST['username'];
+         $admin=isset($_POST['admin'])?1:0;
+         $email=$_POST['email'];
+         $password=$_POST['password'];
+        }}
+        
+    function loginUser($user){
+      $_SESSION['id']=$user['id'];
+      $_SESSION['username']=$user['username'];
+      $_SESSION['admin']=$user['admin'];
+      $_SESSION['message']='You are now logged in';
+      $_SESSION['type']='success';
+ 
+      if($_SESSION['username']){
+         header('location:'.BASE_URL.'/dashboard.php');
+      }else{
+         header('location:'.BASE_URL.'/index.php');
+      }
+      
+      exit();
+
+    }
+   
+     
+
+ 
+    if( isset($_POST['edit-users']) ){
+      $errors=validateUser($_POST);
+      
+       if(count($errors)==0) {
+         $id=$_POST['id'];
+           unset($_POST['edit-users'],$_POST['passwordConf'],$_POST['id']);
+           $_POST['password']=password_hash($_POST['password'],PASSWORD_DEFAULT);
+   
+            $_POST['admin']=isset($_POST['admin'])?1:0;;
+            $count=update($table,$id,$_POST);
+            $_SESSION['message']=' User updated';
+            $_SESSION['type']="success";
+            header("location: ".BASE_URL.'/admin/users/index.php');
+            exit();
+   
+       }
+           else{
+            $username=$_POST['username'];
+            $admin=isset($_POST['admin'])?1:0;
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+           }};
+         
+
+
+ if(isset($_GET['id'])){
+   $user=selectOne($table,['id'=>$_GET['id']]);
+   $id=$user['id'];
+   $username=$user['username'];
+   $admin=isset($user['admin'])?1:0;
+   $email=$user['email'];
+  
+
+ }
+     
+
  if(isset($_POST['login-btn'])){
    $errors=validateLogin($_POST);
 
-   if(count($errors)===0){
+   if(count($errors)==0){
     $user=selectOne($table,['username'=>$_POST['username']]);
     if($user && password_verify($_POST['password'],$user['password'])) {
         $_SESSION['id']=$user['id'];
@@ -75,6 +164,16 @@ if( isset($_POST['register-btn']) ){
         array_push($errors,'Wrong password');
     }
    }
+ }
+
+ if(isset($_GET['delete_id'])){
+   $id=$_GET['delete_id'];
+   $count=delete($table,$id);
+   $_SESSION['message']='Admin user deleted';
+   $_SESSION['type']='error';
+   header("location: ".BASE_URL.'/admin/users/index.php');
+   exit();
+
  }
 
 ?>
